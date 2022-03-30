2bfaf9bcaafa2497fb86badad28ecac6d720460d
Juliana Connors <juliana.connors.dev@gmail.com>
2022-03-30 09:03:27 -0400

    create view model class with properties

--- NewYorkPublicLibrary/Data/BookViewModel.swift
+++ NewYorkPublicLibrary/Data/BookViewModel.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+class BookViewModel {
+    let model: BookModel?
+    
+    init(model: BookModel) {
+        self.model = model
+    }
+    
+    var title: String {
+        model?.title ?? "Title not available"
+    }
+    
+    var authorName: String {
+        model?.authorName?.first ?? "Author name not available"
+    }
+    
+    var yearPublished: String {
+        if let year = model?.firstPublishYear {
+            return String(year)
+        } else {
+            return "Year published not available"
+        }
+    }
+}
